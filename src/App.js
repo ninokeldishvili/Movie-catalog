802fd42857729ef51e75a9ecaf789b4709c86663
Nino Keldishvili <ninokeldishvili@gmail.com>
2020-04-25 22:14:28 +0400

    Silly solution of movie names translate in geo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
 import React from 'react';
 import Movie from './components/Movie/Movie'
-import LanguageSwitch from './components/LanguageSwitch/LanguageSwitch'
 import MoviesData from '../src/Data/MoviesData'
+import MoviesData_Geo from '../src/Data/MoviesData_Geo'
+import Language from './components/Language/Language'
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      MoviesData : MoviesData,
+      lang : 'en',
+    }
+    this.updateDataByLanguage = this.updateDataByLanguage.bind(this);
+  }
+
+  updateDataByLanguage(lang){
+    //console.log("hi")
+       if (lang==='en')
+         this.setState({MoviesData: MoviesData});
+       else 
+         this.setState({MoviesData: MoviesData_Geo});
+        
+  }
+
+  render(){
+    return (
+      <div className="App">
           <header className="App-header">
-              <LanguageSwitch></LanguageSwitch>
-              <h1>What to watch</h1>
+            {/* <Language lang="GEO" id="ka"></Language>
+            <Language lang="ENG" id="en"></Language> */}
+  
+          <div className="lang-switch">
+              <span onClick={this.updateDataByLanguage.bind(this,'ka')}>GEO</span >/<span onClick={this.updateDataByLanguage.bind(this,'en')}>ENG</span>
+          </div>
+  
+          <h1>What to watch</h1>
           </header>
           <div>
-              {MoviesData.map(movie =><Movie key={movie.id} movieData={movie}></Movie>)}
+            {this.state.MoviesData.map(movie =><Movie key={movie.id} movieData={movie}></Movie>)}
           </div>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Data/MoviesData_Geo.js
+++ src/Data/MoviesData_Geo.js
@@ -1,63 +1,63 @@
-const MoviesData = [{
+const MoviesData_Geo = [{
   "id": 1,
   "src": "https://m.media-amazon.com/images/M/MV5BMTlkMmVmYjktYTc2NC00ZGZjLWEyOWUtMjc2MDMwMjQwOTA5XkEyXkFqcGdeQXVyNTI4MzE4MDU@._V1_UX182_CR0,0,182,268_AL_.jpg",
   "rating": 9.7,
-  "name": "Rohan and Runo"
+  "name": "როჰანი და რუნო"
 }, {
   "id": 2,
   "src": "https://m.media-amazon.com/images/M/MV5BZTNiY2FiYzktNWNmMi00YjMzLTkwMWItMGE3ZDc5OWYzODIyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UX182_CR0,0,182,268_AL_.jpg",
   "rating": 8.1,
-  "name": "Marvin-Lindgren"
+  "name": "მარვინ ლინდგრენი"
 }, {
   "id": 3,
   "src": "https://m.media-amazon.com/images/M/MV5BOTMyYTIyM2MtNjQ2ZC00MWFkLThhYjQtMjhjMGZiMjgwYjM2XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UX182_CR0,0,182,268_AL_.jpg",
   "rating": 8.6,
-  "name": "Stehr-Champlin"
+  "name": "სტეარ ჩაპლინი"
 }, {
   "id": 4,
   "src": "https://m.media-amazon.com/images/M/MV5BMmJkM2M0OTUtNGI3NC00ZTNiLWI4MDUtMGI5MzM1MmQyNTliXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_UX182_CR0,0,182,268_AL_.jpg",
   "rating": 8.3,
-  "name": "Schmitt-Cassin"
+  "name": "შმიტ კასინი"
 }, {
   "id": 5,
   "src": "https://m.media-amazon.com/images/M/MV5BMTQ0NjUzMDMyOF5BMl5BanBnXkFtZTgwODA1OTU0MDE@._V1_UX182_CR0,0,182,268_AL_.jpg",
   "rating": 9.7,
-  "name": "McKenzie-Walter"
+  "name": "მაკკენზი ვალტერი"
 }, {
   "id": 6,
   "src": "https://m.media-amazon.com/images/M/MV5BOTJiNDEzOWYtMTVjOC00ZjlmLWE0NGMtZmE1OWVmZDQ2OWJhXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_UX182_CR0,0,182,268_AL_.jpg",
   "rating": 9.8,
-  "name": "King-Sauer"
+  "name": "კინგ საუერი"
 }, {
   "id": 7,
   "src": "https://m.media-amazon.com/images/M/MV5BMGUwZjliMTAtNzAxZi00MWNiLWE2NzgtZGUxMGQxZjhhNDRiXkEyXkFqcGdeQXVyNjU1NzU3MzE@._V1_UX182_CR0,0,182,268_AL_.jpg",
   "rating": 10.0,
-  "name": "Runolfsdottir Inc"
+  "name": "რუნოლფდოტი ინქ"
 }, {
   "id": 8,
   "src": "https://m.media-amazon.com/images/M/MV5BMmEzNTkxYjQtZTc0MC00YTVjLTg5ZTEtZWMwOWVlYzY0NWIwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg",
   "rating": 7.8,
-  "name": "Konopelski and Morar"
+  "name": "კონოპლესკი და მორა"
 }, {
   "id": 9,
   "src": "https://m.media-amazon.com/images/M/MV5BMDliMmNhNDEtODUyOS00MjNlLTgxODEtN2U3NzIxMGVkZTA1L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg",
   "rating": 8.3,
-  "name": "Lub and Funk"
+  "name": "ლუბი და ფანკი"
 }, {
   "id": 10,
   "src": "https://m.media-amazon.com/images/M/MV5BMTg0NTIzMjQ1NV5BMl5BanBnXkFtZTcwNDc3MzM5OQ@@._V1_UX182_CR0,0,182,268_AL_.jpg",
   "rating": 8.3,
-  "name": "Sons"
+  "name": "ვაჟები"
 }, {
   "id": 11,
   "src": "https://m.media-amazon.com/images/M/MV5BYWIyODUxOTItOTYwNS00Mjc4LWI4ZjUtNWQ2YWVmNWQ1MmViXkEyXkFqcGdeQXVyMjg2MTMyNTM@._V1_UY268_CR147,0,182,268_AL_.jpg",
   "rating": 7.5,
-  "name": "Brown-Johns"
+  "name": "ყავისფერი ჯონები"
 }, {
   "id": 12,
   "src": "https://m.media-amazon.com/images/M/MV5BOTcwNzU2MGEtMzUzNC00MzMwLWJhZGItNDY3NDllYjU5YzAyXkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_UX182_CR0,0,182,268_AL_.jpg",
   "rating": 7.6,
-  "name": "Pollich-Moen"
+  "name": "პოლიხ მოენი"
   },]
 
-export default MoviesData;
+export default MoviesData_Geo;
