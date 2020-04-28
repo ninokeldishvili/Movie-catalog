c14b9fe9d39280e436ddca232eb10a9100e6a37d
Nino Keldishvili <ninokeldishvili@gmail.com>
2020-04-29 01:51:25 +0400

    Add VIP poster and styling

--- src/App.js
+++ src/App.js
@@ -2,47 +2,49 @@ import React from 'react';
 import Movie from './components/Movie/Movie'
 import MoviesData from '../src/Data/MoviesData'
 import MoviesData_Geo from '../src/Data/MoviesData_Geo'
-import Language from './components/Language/Language'
+import VIPPoster from './components/VIPPoster/VIPPoster'
 import "./App.css"
+import './components/Language/Language.css'
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      MoviesData : MoviesData,
-      lang : 'en',
+    constructor(props) {
+        super(props);
+        this.state = {
+            MoviesData: MoviesData,
+            lang: 'en',
+        }
+        this.updateDataByLanguage = this.updateDataByLanguage.bind(this);
     }
-    this.updateDataByLanguage = this.updateDataByLanguage.bind(this);
-  }
 
-  updateDataByLanguage(lang){
-    //console.log("hi")
-       if (lang==='en')
-         this.setState({MoviesData: MoviesData});
-       else 
-         this.setState({MoviesData: MoviesData_Geo});
-        
-  }
+    updateDataByLanguage(lang) {
+        //console.log("hi")
+        if (lang === 'en')
+            this.setState({ MoviesData: MoviesData });
+        else
+            this.setState({ MoviesData: MoviesData_Geo });
 
-  render(){
-    return (
-      <div className="App">
-          <header className="App-header">
-            {/* <Language lang="GEO" id="ka"></Language>
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <VIPPoster></VIPPoster>
+                <header className="App-header">
+                    {/* <Language lang="GEO" id="ka"></Language>
             <Language lang="ENG" id="en"></Language> */}
-  
-          <div className="lang-switch">
-              <span onClick={this.updateDataByLanguage.bind(this,'ka')}>GEO</span >/<span onClick={this.updateDataByLanguage.bind(this,'en')}>ENG</span>
-          </div>
-  
-          <h1>What to watch</h1>
-          </header>
-          <div>
-            {this.state.MoviesData.map(movie =><Movie key={movie.id} movieData={movie}></Movie>)}
-          </div>
-      </div>
-    );
-  }
+
+                    <div className="lang-switch">
+                        <span onClick={this.updateDataByLanguage.bind(this, 'ka')}>GEO</span >/<span onClick={this.updateDataByLanguage.bind(this, 'en')}>ENG</span>
+                    </div>
+
+                    <h1>What to watch</h1>
+                </header>
+                <div>
+                    {this.state.MoviesData.map(movie => <Movie key={movie.id} movieData={movie}></Movie>)}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/VIPPoster/VIPPoster.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import poster from '../../images/poster.jpg'
+import './VIPPoster.css'
+
+
+
+const VIPPoster = function () {
+    return (
+        <div>
+            <div className="poster-container">
+                <div className="poster-lables-container">
+                    <h1>Bohemian Rapsody</h1>
+                    <button className="watch-now-btn">Watch Now</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default VIPPoster
